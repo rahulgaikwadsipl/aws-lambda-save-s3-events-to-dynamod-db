33aec5f9e53488e474b11b99620652ea32704ea5
rahulgaikwadsipl <rahul.gaikwad@systematixindia.com>
2018-08-02 13:02:27 +0530

    Add files via upload
    First commit

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import boto3
+from uuid import uuid4
+
+dynamodb = boto3.resource('dynamodb')
+
+def lambda_handler(event,context):
+        print (event)
+        for record in event['Records']:
+              bucket_name = record['s3']['bucket']['name']
+              object_key = record['s3']['object']['key']
+              size = record['s3']['object'].get('size', -1)
+              event_name = record['eventName']
+              event_time = record['eventTime']
+              dynamodb.Table('my-table').put_item(Item={'RequestId':str(uuid4()),'Bucket': bucket_name, 'Object': object_key, 'Size':    size, 'Event': event_name, 'EventTime':event_time } )
+
