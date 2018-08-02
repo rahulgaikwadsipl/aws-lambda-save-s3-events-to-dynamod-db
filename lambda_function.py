70ada953f1ffec6933aaba6a28be64129b23ad89
rahulgaikwadsipl <rahul.gaikwad@systematixindia.com>
2018-08-02 13:04:44 +0530

    Update lambda_function.py
    comments added

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3 #you need to download boto3 sdk in same folder of you function
 from uuid import uuid4
 
 dynamodb = boto3.resource('dynamodb')
@@ -12,4 +12,4 @@ def lambda_handler(event,context):
               event_name = record['eventName']
               event_time = record['eventTime']
               dynamodb.Table('my-table').put_item(Item={'RequestId':str(uuid4()),'Bucket': bucket_name, 'Object': object_key, 'Size':    size, 'Event': event_name, 'EventTime':event_time } )
-
+              #here you need to put your dynamoDB table name in place of my-table  
